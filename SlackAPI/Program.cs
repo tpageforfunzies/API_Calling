90cbbeb863c65d43cd46ba008bef1a58f2f83d59
Taylor Page <taylor.jay.page@gmail.com>
2017-12-01 14:01:20 -0500

    got a 302!

--- SlackAPI/Program.cs
+++ SlackAPI/Program.cs
@@ -21,49 +21,43 @@ namespace SlackAPI
         //       main is just the runasync
         //       redirecturi would be "localhost" or 127.0.0.1:port/
 
-        static string clientId = "252005972915.279953222963";
-        static string clientSecret = "b0fca759029960ac2e214636da305f76";
+        public static string BearerToken { get; set; }
+        private const string _apiUrl = "https://slack.com/";
+        private const string _clientId = "252005972915.279953222963";
+        private const string _clientSecret = "b0fca759029960ac2e214636da305f76";
+        private const string _redirectUri = "http://localhost";
+        private const string _scope = "chat:write:user";
 
-        static string baseUrl = "https://slack.com/";
+        static HttpClient client = new HttpClient();
 
-        static string accessToken = null;
+        static async Task<HttpResponseMessage> Auth()
+        {
+            string url = $"{_apiUrl}/oauth/authorize/?";
+
+            string requestString = $"client_id={_clientId}&scope={_scope}" +
+                $"redirect_uri={_redirectUri}";
+
+            var response = await client.GetAsync($"{url} + {requestString}");
+            Console.WriteLine(response);
+
+            return response;
+        }
 
         static void Main(string[] args)
         {
-
-            DoIt().Wait();
-
+            RunAsync().GetAwaiter().GetResult();
         }
 
-        private static async Task<int> DoIt()
+        static async Task RunAsync()
         {
-            accessToken = await GetAccessToken();
-            Console.WriteLine(accessToken != null ? "Got Token" : "No Token found");
-            return 0;
-        }
+            //client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(
+            //    new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("" +
+            //    "application/json"));
 
-        private static async Task<string> GetAccessToken()
-        {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri(baseUrl);
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            var result = await Auth();
+            Console.WriteLine(result);
 
-                List<KeyValuePair<string, string>> postData = new List<KeyValuePair<string, string>>();
-                postData.Add(new KeyValuePair<string, string>("client_id", clientId));
-                postData.Add(new KeyValuePair<string, string>("scope", "chat:write:user"));
-                postData.Add(new KeyValuePair<string, string>("redirect_uri", "209.43.102.90"));
-
-                FormUrlEncodedContent content = new FormUrlEncodedContent(postData);
-
-                HttpResponseMessage response = await client.PostAsync("oauth/authorize", content);
-                string jsonString = await response.Content.ReadAsStringAsync();
-                object responseData = JsonConvert.DeserializeObject(jsonString);
-                Console.WriteLine(((dynamic)responseData).code);
-                return ((dynamic)responseData).code;
-                
-            }
         }
 
     }
