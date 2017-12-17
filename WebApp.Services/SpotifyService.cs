957592f0ee10ec848c3215ca2c817d2db8ac6ed3
tpageforfunzies <tjpage@indiana.edu>
2017-12-17 16:17:39 -0500

    set up spotify api web app

---
+++ WebApp.Services/SpotifyService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApp.Services
+{
+    public class SpotifyService
+    {
+    }
+}
---
+++ WebApp/Controllers/SpotifyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class SpotifyController : Controller
+    {
+        // GET: Spotify
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Auth()
+        {
+            return View();
+        }
+    }
+}
